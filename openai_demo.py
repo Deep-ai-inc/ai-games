fb9e05519d11c950f596cb240992c1529b7a7554
Kevin Baragona <kmbaragona@gmail.com>
2023-05-22 23:12:20 -0400

    updates

--- openai_demo.py
+++ openai_demo.py
@@ -9,7 +9,21 @@ for line in file:
     if line[0] != '#':
         exec(line)
 
-chatHistory = [{"role": "user", "content": "Write a Haiku about cats."}]
+chatHistory = [{"role": "user", "content": '''You are a role-playing dungeon game. At each step, describe what is happening in 1 to 3 sentences. Then give the player 4 options on how to proceed. One of the options should be logical, one cunning, one aggressive, and one completely ridiculous. Return the options as a list of strings. Start the game by describing who the player is and where they are, what they see.
+Also describe what the user sees in a sentence, this is the image caption.
+The output after each turn (including the starting turn) should be a JSON object with the keys: "description", "choices", "image caption".
+This is a list of other characters they might encounter during the game:
+
+
+[
+  {"name": "Aelar Rivenhart", "bio": "A master archer and Elf princess who takes command of the wind and strikes every weakness of her enemies."},
+  {"name": "Cynthia Brighthelm", "bio": "A quirky Human mage with a happy demeanor, turning her adversaries' spells against them."},
+  {"name": "Argis the Bold", "bio": "King of the Dwarves, Argis fights with honor and commands his people with his indestructible hammer."},
+  {"name": "Xirala Shadowdancer", "bio": "A sultry Tiefling rogue who can shift between the shadows, stealing without a trace."},
+  {"name": "Baelgor Earthsplitter", "bio": "A wise Gnome druid with a deep connection to nature, wielding the earth itself as a weapon."},
+  {"name": "Elara Nightstar", "bio": "A celestial Aasimar Cleric called to heal and protect the faithful, her radiance blinding enemies."},
+  {"name": "Fizzlewick McFlambe", "bio": "An impulsive Halfling pyromancer who sets fire to the battlefield, leaving a trail of ashes in his wake."},
+]'''}]
 
 resp = requests.post(
         "https://api.openai.com/v1/chat/completions",
@@ -36,3 +50,4 @@ print("response from openai: ", response)
 
 # get actual text from response:
 text = response['choices'][0]['message']['content']
+print(text)
--- server.py
+++ server.py
@@ -131,8 +131,7 @@ def upscale_api():
     return jsonify({'image_url': image_url})
 
 # Call like this:
-# curl -X POST -d '{"text":"write haiku"}' -H 'Content-Type: application/json' http://127.0.0.1:5432/text_api
-
+# curl -X POST -d '{"chatHistory":[{"role": "user", "content":"write haiku about dogs"}]}' -H 'Content-Type: application/json' http://127.0.0.1:5432/text_api
 # run the app
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5432)
